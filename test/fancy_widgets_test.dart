9cea1e5b8f8cee982a14fd534040b18f98149596
Saleh Ghaleb <ghaleb.s@gust.edu.kw>
2019-01-21 09:21:59 +0300

    initial release

--- test/fancy_widgets_test.dart
+++ test/fancy_widgets_test.dart
@@ -6,24 +6,19 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutfire/main.dart';
+import 'package:fancy_widgets/fancy_widgets.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.pumpWidget(RoundIconButton(      
+      onPressed: null,
+      icon: Icons.content_copy,
+    ));
+    
+    // Tap the icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.content_copy));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    
   });
 }
